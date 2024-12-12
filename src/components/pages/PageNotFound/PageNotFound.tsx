0aaa74db8b1b951b6ef7b41633ef988ff4e1b63c
KartoffelChips <jan@strassburger.org>
2024-12-12 22:33:50 +0100

    added 404 page

--- src/components/pages/PageNotFound/PageNotFound.tsx
+++ src/components/pages/PageNotFound/PageNotFound.tsx
@@ -1,7 +1,23 @@
+import { HouseChimneyBlank } from "dazzle-icons";
+import Header from "../../layout/Header";
+import "./PageNotFound.scss";
+import { Link } from "wouter";
+
 const PageNotFound = () => {
     return (
         <>
-            <h1>Page Not Found</h1>
+            <Header />
+            <main className={"pageNotFound"}>
+                <img src={"/fzf-cat.png"} className={"cat"} />
+                <h2>Page Not Found</h2>
+                <Link
+                    className={"button"}
+                    href={"/"}
+                >
+                    <HouseChimneyBlank />
+                    Home
+                </Link>
+            </main>
         </>
     )
 }
